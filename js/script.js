816f48508b5ace875e70f0de81ca6b02016c0989
efbraniii <64694974+efbraniii@users.noreply.github.com>
2020-12-08 21:42:37 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-var modal = document.getElementById('id01');
+$(function(){
+
+  var modal = document.getElementById('id01');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-$(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
+$(".navbar-collapse a").click(function(){
+  $(".navbar-collapse").collapse('hide');
+});
+});
+
